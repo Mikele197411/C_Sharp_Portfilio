66ceb0cae93f9b76c1180ac93a36b6da655d3842
Mikhail Shilkov <mikhailshilkovriga@gmail.com>
2022-09-17 15:28:14 +0300

    2- small fixes

--- ConsoleImportDBToCSV/ExtendedClasses/Logger.cs
+++ ConsoleImportDBToCSV/ExtendedClasses/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleImportDBToCSV.ExtendedClasses
 {
-    class Logger
+   public class Logger
     {
+        private static readonly ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
     }
 }
